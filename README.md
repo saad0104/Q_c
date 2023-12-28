7cd7f95e461d8518ad34078bbd4ed585d4cd9063
emon4075 <mdabdullahemon4075@gmail.com>
2023-12-29 02:11:57 +0600

    First Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<b><h3>It a collective effort by some passionate tech addicts from CU CSE-23. Our aim is to solve questions which are given as task by our respective teachers.</h3></b>
+<h1>Tools Used:</h1>
+<p>
+  <ul>
+    <li>NVim</li>
+    <li>Vim</li>
+    <li>Git & GitHub</li>
+  </ul>
+</p>
---
+++ RED-MARKED/Section-A/1_b.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+    printf("Size of Integer Data Type: %d Bytes\n", sizeof(int));
+    printf("Size of Float Data Type: %d Bytes\n", sizeof(float));
+    printf("Size of Double Data Type: %d Bytes\n", sizeof(double));
+    printf("Size of Character Data Type: %d Bytes\n", sizeof(char));
+    printf("Size of Integer Data Type: %d Bytes\n", sizeof(bool));
+    return 0;
+}
