9b554de754f352794b832a730afde97cfd39218d
emon4075 <mdabdullahemon4075@gmail.com>
2023-12-29 09:38:23 +0600

    Corrected The Code

--- RED-MARKED/Section-A/1_c.c
+++ RED-MARKED/Section-A/1_c.c
@@ -1,38 +1,37 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int A[10],O[10],E[10];
-    int i,j,k, n;
+    int A[10], O[10], E[10];
+    int i, j = 0, k = 0, n;
     printf("Please Enter the size of an array:\n");
-    scanf("%d",&n);
-    printf("Enter %d element:\n",n);
-    for(i=0; i<n; i++)
+    scanf("%d", &n);
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++)
     {
-        scanf("%ld", &A[i]);
-        fflush(stdin);
+        scanf("%d", &A[i]);
     }
-    for(i=0; i<n; i++)
+    for (i = 0; i < n; i++)
     {
-        if(A[i]%2==0)
+        if (A[i] % 2 == 0)
         {
-            E[j]=A[i];
+            E[j] = A[i];
             j++;
         }
         else
         {
-            O[k]=A[i];
+            O[k] = A[i];
             k++;
         }
     }
-    printf("Odd elememt array is:\n");
-    for(i=0; i<j; i++)
+    printf("Even element array is:\n");
+    for (i = 0; i < j; i++)
     {
-        printf("%ld\t",E[i]);
+        printf("%d ", E[i]);
     }
-    printf("\nOdd elememt array is:\n");
-    for(i=0; i<k; i++)
+    printf("\nOdd element array is:\n");
+    for (i = 0; i < k; i++)
     {
-        printf("%ld\t",O[i]);
+        printf("%d ", O[i]);
     }
     return 0;
 }
