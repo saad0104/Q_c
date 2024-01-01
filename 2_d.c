35452ff7fb536be46ac22fc3658bd8ca565c6aac
saad <saad.csecu@gmail.com>
2024-01-02 00:11:42 +0600

    Messi reporting

---
+++ 2_d.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+ int i=1,j=1;
+
+  while(i<=5)
+  {
+    while(j<=i)
+    {
+
+      printf("%i",i);
+      j++;
+    }
+  printf("\n");
+  i++;
+  }
+  
+
+
+
+  return 0;
+}
+
