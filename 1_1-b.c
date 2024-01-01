0ee2179bcc32c2aa526ab5321c6d741f305186cd
saad <saad.csecu@gmail.com>
2024-01-02 00:18:51 +0600

    Messi reporting

---
+++ 1_1-b.c
@@ -0,0 +1,18 @@
+//1(b)
+//size of data types 
+//%lu=unsigned long integer
+
+#include<stdio.h>
+int main()
+{
+printf("Size of Character %lu byte\n", sizeof(char));
+printf("Size of integer: %lu byte\n", sizeof(int));
+printf("Size of Long Integer %lu byte\n", sizeof(long));
+printf("Size of Float %lu byte\n", sizeof(float)); 
+printf("Size of Double %lu byte\n", sizeof(double)); 
+
+
+return 0;
+}
+
+
